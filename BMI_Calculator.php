df2782e142d6750bd7b7cb7a075beea5c4cbe388
LaibaRazi <khanlayba72@gmail.com>
2023-02-03 14:08:38 +0500

    BMI Calculator height in meter & weight in kgs

---
+++ BMI_Calculator.php
@@ -0,0 +1,24 @@
+<?php
+$height=1.5;
+$weight=55;
+$Bmi_cal=($weight/($height*$height));
+if ($Bmi_cal<=18.5) {
+    echo("Underweight");
+}
+elseif($Bmi_cal>=18.5 && $Bmi_cal<=24.9)
+{
+    echo("Normal Weight");
+}
+elseif ($Bmi_cal>=25 && $Bmi_cal<=29.9) {
+    echo("Over Weight");
+    
+}
+elseif ($Bmi_cal>=30) {
+    echo("Obesity");
+    
+}
+else
+{
+    echo("error");
+}
+?>
